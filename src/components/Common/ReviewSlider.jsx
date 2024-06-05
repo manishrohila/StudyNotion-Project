ac5b2a2aaf6eae5a02269fcdec90aaeb2b86cf31
manishrohila <manishrohila0507@gmail.com>
2024-06-06 00:59:40 +0530

    Project uploaded

---
+++ src/components/Common/ReviewSlider.jsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from "react"
+import ReactStars from "react-rating-stars-component"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
+import "../../App.css"
+// Icons
+import { FaStar } from "react-icons/fa"
+// Import required modules
+import { Autoplay, FreeMode, Pagination } from "swiper"
+
+// Get apiFunction and the endpoint
+import { apiConnector } from "../../services/apiConnector"
+import { ratingsEndpoints } from "../../services/apis"
+
+function ReviewSlider() {
+
+   //responsive start
+   const [slidesPerView, setSlidesPerView] = useState(4);
+  const [currentW, setCurrentW]=useState(getCurrentW());
+
+  function getCurrentW(){
+    return {
+        width: window.innerWidth,
+        
+    }
+  }
+  
+
+  useEffect(()=>{
+    const updateDimension = () => {
+      setCurrentW(getCurrentW());
+  }
+  window.addEventListener('resize', updateDimension);
+
+  if(currentW.width<=607)
+  {
+   setSlidesPerView(1);
+  }
+  else if(currentW.width>607 && currentW.width<1100)
+  {
+    setSlidesPerView(2);
+  }
+  else
+  setSlidesPerView(4);
+  return(() => {
+
+
+    window.removeEventListener('resize', updateDimension);
+})
+
+ 
+
+  
+
+  },[currentW])
+
+  //responsive end
+
+  const [reviews, setReviews] = useState([])
+  const truncateWords = 15
+
+  useEffect(() => {
+    ;(async () => {
+      const { data } = await apiConnector(
+        "GET",
+        ratingsEndpoints.REVIEWS_DETAILS_API
+      )
+      if (data?.success) {
+        setReviews(data?.data)
+      }
+    })()
+  }, [])
+
+  // console.log(reviews)
+
+  return (
+    <div className="text-white w-[90%]">
+     
+      <div className="my-[50px] h-[184px] max-w-maxContentTab lg:max-w-maxContent">
+        <Swiper
+          slidesPerView={slidesPerView}
+          spaceBetween={25}
+          loop={true}
+          freeMode={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[FreeMode, Pagination, Autoplay]}
+          className="w-full "
+        >
+          {reviews.map((review, i) => {
+            return (
+              <SwiperSlide key={i}>
+                <div className="flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25">
+                  <div className="flex items-center gap-4">
+                    <img
+                      src={
+                        review?.user?.image
+                          ? review?.user?.image
+                          : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`
+                      }
+                      alt=""
+                      className="h-9 w-9 rounded-full object-cover"
+                    />
+                    <div className="flex flex-col">
+                      <h1 className="font-semibold text-richblack-5">{`${review?.user?.firstName} ${review?.user?.lastName}`}</h1>
+                      <h2 className="text-[12px] font-medium text-richblack-500">
+                        {review?.course?.courseName}
+                      </h2>
+                    </div>
+                  </div>
+                  <p className="font-medium text-richblack-25">
+                    {review?.review.split(" ").length > truncateWords
+                      ? `${review?.review
+                          .split(" ")
+                          .slice(0, truncateWords)
+                          .join(" ")} ...`
+                      : `${review?.review}`}
+                  </p>
+                  <div className="flex items-center gap-2 ">
+                    <h3 className="font-semibold text-[#d9cd4a]">
+                      {review.rating.toFixed(1)}
+                    </h3>
+                    <ReactStars
+                      count={5}
+                      value={review.rating}
+                      size={20}
+                      edit={false}
+                      activeColor="#ffd700"
+                      emptyIcon={<FaStar />}
+                      fullIcon={<FaStar />}
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+            )
+          })}
+          {/* <SwiperSlide>Slide 1</SwiperSlide> */}
+        </Swiper>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewSlider
